4928ac5f3fc12987d5aeefbeca109d206388c0c5
Felipe Venas <felipevenas3003@gmail.com>
2025-10-13 23:09:39 -0300

    Adiciona arquivos iniciais para a estrutura do projeto, incluindo modelos, repositórios e serviços de usuário e link.

---
+++ app/domain/user/model.py
@@ -0,0 +1,11 @@
+from app.db.base_class import Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+class User(Base):
+
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    nome = Column(String(70), nullable=False)
+    email = Column(String(100), nullable=False)
+    login = Column(String(20), nullable=False)
+    senha = Column(String(20), nullable=False)
